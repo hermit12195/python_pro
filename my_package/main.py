a3087f668c869ad30f71286a4e620fc994e98436
Vlad <96884397+hermit12195@users.noreply.github.com>
2025-02-01 11:07:44 +0200

    Delete my_package directory

--- my_package/main.py
+++
@@ -1,7 +0,0 @@
-from math_utils import factorial, common_divisor
-from string_utils import up, remove_space
-
-print(factorial(5))
-print(common_divisor(15, 5))
-print(up("test"))
-print(remove_space("   test   "))
--- my_package/string_utils.py
+++
@@ -1,6 +0,0 @@
-def up(string):
-    return string.upper()
-
-def remove_space(string):
-    return string.strip()
-
