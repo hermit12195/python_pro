35e1f686667ab2d7003577ba1a5305e9895af242
Vlad <96884397+hermit12195@users.noreply.github.com>
2025-02-01 11:10:02 +0200

    Add files via upload

---
+++ my_package/math_utils.py
@@ -0,0 +1,36 @@
+def factorial(num):
+    """
+    Calculates the factorial of a number.
+
+    Parameters:
+    num (int): The number for which the factorial is to be calculated.
+
+    Returns:
+    int: The factorial of the number `num`.
+    """
+
+    count = 1
+    for n in range(1, num + 1):
+        count *= n
+    return count
+
+
+def common_divisor(num1, num2):
+    """
+    Calculates the greatest common divisor (GCD) of two numbers using the Euclidean algorithm.
+
+    Parameters:
+    num1 (int): The first number.
+    num2 (int): The second number.
+
+    Returns:
+    int: The greatest common divisor (GCD) of `num1` and `num2`.
+    """
+
+    if num1 % num2 == 0:
+        return num2
+    else:
+        return common_divisor(num2, num1 % num2)
+
+
+
---
+++ my_package/string_utils.py
@@ -0,0 +1,26 @@
+def up(string):
+    """
+    Converts all characters in a string to uppercase.
+
+    Parameters:
+    string (str): The string to be converted to uppercase.
+
+    Returns:
+    str: The uppercase version of the input string.
+    """
+
+    return string.upper()
+
+def remove_space(string):
+    """
+    Removes leading and trailing spaces from a string.
+
+    Parameters:
+    string (str): The string from which leading and trailing spaces will be removed.
+
+    Returns:
+    str: The string with leading and trailing spaces removed.
+    """
+
+    return string.strip()
+
