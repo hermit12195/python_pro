d5f17890cf4598b3a60330ed012ba3c4031fdb61
Vlad <96884397+hermit12195@users.noreply.github.com>
2025-04-28 13:44:36 +0300

    Delete DjangoBlog directory

--- DjangoBlog/chats/ws/utils.py
+++
@@ -1,8 +0,0 @@
-from channels.db import database_sync_to_async
-
-from blog.models import BlogUser
-
-@database_sync_to_async
-def get_email(username):
-    user=BlogUser.objects.get(username=username)
-    return user.email
